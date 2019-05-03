62cb961a955da1566101d2d6ed268f4f0ce812bb
Adrianus Kleemans <a.kleemans@gmail.com>
2019-05-03 22:08:36 +0200

    add LevelSet and level, changing way levels get fetched

---
+++ src/objects/level-set.ts
@@ -0,0 +1,6 @@
+export enum LevelSet {
+    set1,
+    set2,
+    set3,
+    set4
+}
---
+++ src/objects/level.ts
@@ -0,0 +1,14 @@
+import {LevelSet} from "./level-set";
+
+export class Level {
+
+    public readonly baseGrid: string[];
+    public readonly levelSet: LevelSet;
+    public readonly nr: number;
+
+    public constructor(baseGrid: string[], levelSet: LevelSet, nr: number) {
+        this.baseGrid = baseGrid;
+        this.levelSet = levelSet;
+        this.nr = nr;
+    }
+}
